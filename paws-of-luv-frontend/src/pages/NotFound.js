314ae03193ef6bfc59111cdd0fc23d57f39cabf5
LEARN Academy <devjoelmichael@protonmail.com>
2020-10-20 13:02:09 -0700

    trying to make photos work

--- paws-of-luv-frontend/src/pages/NotFound.js
+++ paws-of-luv-frontend/src/pages/NotFound.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import test2 from '../assets/test2.png';
 
 class NotFound extends Component {
     render() {
         return (
             <>
                 <h1>Oopsies.</h1>
-                <div className="opps">
-
-                </div>
+                <img src="../assets/test2.png" alt="404 Page Not Found" className="cat" />
             </>
         )
     }
