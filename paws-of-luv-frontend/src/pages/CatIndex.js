27fa142f82f03c7e773b1bf83e5486b95f710f2c
bnddiep <bach.nhu.diep@gmail.com>
2020-10-20 14:25:50 -0700

    added the cards for the cats

--- paws-of-luv-frontend/src/pages/CatIndex.js
+++ paws-of-luv-frontend/src/pages/CatIndex.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import {
+    Card,
+    CardTitle,
+    Col
+} from 'reactstrap'
 
 class CatIndex extends Component {
     render() {
     return (
         <>
             <h3>Cat Index</h3>
+            <br />
+            <Col sm="6">
+                { this.props.cats.map ((cat, index) => {
+                    return (
+                        <Card key={ index }>
+                            <CardTitle>
+                                <h3> { cat.name } </h3>
+                            </CardTitle>
+                        </Card>
+                    )
+                })}
+            </Col>
         </>
         )
     }
--- paws-of-luv-frontend/src/pages/CatShow.js
+++ paws-of-luv-frontend/src/pages/CatShow.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import {
+    Card,
+    CardTitle,
+    CardText,
+    Col
+} from 'reactstrap'
 
 class CatShow extends Component {
     render() {
     return (
         <>
             <h3>Cat Show</h3>
+            <Col sm="6">
+                <Card>
+                    <CardTitle>
+                        { this.props.cat.name }
+                    </CardTitle>
+                    <CardText>
+                        Hello, I am { this.props.cat.age } years old. I love { this.props.cat.enjoys }.
+                    </CardText>
+                </Card>
+            </Col>
         </>
         )
     }
