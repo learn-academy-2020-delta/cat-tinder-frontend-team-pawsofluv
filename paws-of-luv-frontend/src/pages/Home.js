892595828caa798718919b53f201e574115c2f90
LEARN Academy <devjoelmichael@protonmail.com>
2020-10-20 15:43:35 -0700

    A little more styling

--- paws-of-luv-frontend/src/pages/Home.js
+++ paws-of-luv-frontend/src/pages/Home.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import pawPrint from '../assets/pawPrint.png';
+import catFly from '../assets/catFly.jpg';
 
 class Home extends Component {
     render() {
         return (
             <>
                 <div className="homeBackground">
-                    <h1>Paws of Luv</h1>
-                    <p>Here are some Kitty's</p>
-
+                    <div className="titleOne">
+                        <img src={pawPrint} alt="Paws of Luv" className="logoOne" />
+                        <h1>Paws of Luv</h1>
+                        <img src={pawPrint} alt="Paws of Luv" className="logoTwo" />
+                    </div>
+                    <br />
+                    <img src={catFly} alt="Paws of Luv" className="picOne" />
+                    <p className="kitty">Here are some Kitty's</p>
                 </div>
             </>
         )
